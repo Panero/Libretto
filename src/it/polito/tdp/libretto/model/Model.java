9b1c31c3761937828d4ffccf241b00f75456345a
Marcello Panero <Marcello Panero@LAPTOP-RI2N8FNJ.station>
2017-03-28 15:27:02 +0200

    Model

--- src/it/polito/tdp/libretto/model/Model.java
+++ src/it/polito/tdp/libretto/model/Model.java
@@ -1,5 +1,43 @@
 package it.polito.tdp.libretto.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
 
+	private List<Esame> esami;
+	
+	public Model() {
+		this.esami = new ArrayList<Esame>();
+	}
+	
+	/**
+	 * aggiunge un nuovo esame all'elenco esami presenti,
+	 * verificando che non ci sia già
+	 * @param e
+	 */
+	public void addEsame(Esame e){
+		if(!esami.contains(e)){
+		esami.add(e);
+		} else {
+			throw new IllegalStateException("Codice esame già presente");
+		}
+	}
+	
+	/**
+	 * ricerca l'esame tramite il codice.
+	 * Se non c'è restituisce null
+	 * @param codice
+	 * @return
+	 */
+	public Esame trovaEsame(String codice){
+		int pos = esami.indexOf(new Esame(codice,null,null));
+	    if(pos==-1){
+	    	return null;
+	    } else {
+	    	return esami.get(pos);
+	    }
+	}
+	
+	
 }
